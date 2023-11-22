88ef84a8b90d25edd439afa62785bffdf269305b
AboAsil301 <aboasil9459@gmail.com>
2023-11-23 00:04:12 +0200

    update avatar logo

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
-import adminIcon from "../../Image/icon/avatar.png";
+import adminIcon from "../../Image/icon/sayed.png";
 import {
   Navbar,
 } from "reactstrap";
